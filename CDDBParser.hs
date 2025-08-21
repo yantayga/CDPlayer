76c1c63c2ec1001d1127e26644cea66dfe2644e2
yantayga <>
2025-08-21 16:26:07 +0300

    Added some parsers. loading and parsing .cddb file

--- CDDBParser.hs
+++ CDDBParser.hs
@@ -5,6 +5,9 @@ import Text.Parsec.Indent
 import Control.Monad.State
 import Text.Parsec (optional, many)
 import qualified Data.Text as T
+import Data.Time
+import Data.Maybe
+
 
 test :: String
 test = unlines [
@@ -33,8 +36,24 @@ iParse aParser source_name input =
   runIndentParser aParser () source_name input
 
 -- Parser for quoted strings
-noncommentString :: IParser String
-noncommentString = many (noneOf "\"\n#")
+noncommentStringParser :: IParser String
+noncommentStringParser = many (noneOf "\"\n#")
+
+parseBasicDate :: String -> Maybe UTCTime
+parseBasicDate = parseTimeM True defaultTimeLocale "%d.%m.%Y"
+
+dateParser :: IParser (Maybe UTCTime)
+dateParser = do
+    strDate <- noncommentStringParser
+    spaces
+    optional commentParser
+    return $ parseBasicDate strDate
+
+integerParser :: IParser Integer
+integerParser = do
+    sign <- option "" (string "-" <|> string "+")
+    integerPart <- many1 digit
+    return $ read (sign ++ integerPart)
 
 doubleParser :: IParser Double
 doubleParser = do
@@ -58,17 +77,29 @@ commentParser = do
   spaces
   return ""
 
+commaSeparatedparser :: IParser a -> IParser [a]
+commaSeparatedparser parser = sepBy parser (char ',') 
+
 type Term = String
 data Taxonomy = Taxonomy Term [Taxonomy] deriving (Eq, Show)
 
 type Name = String
+type Version = Integer
+type Date = Maybe UTCTime
+
 type Comment = String
 type Score = Double
 type Match = String
 type Actions = String
-data Rule = Rule Name Comment Score Match [Taxonomy] Actions deriving (Eq, Show)
+data Rule = Rule Comment Score Match [Taxonomy] Actions deriving (Eq, Show)
 
-data Rules = Rules Name [Rule] deriving (Eq, Show)
+data Rules = Rules [Rule] deriving (Eq, Show)
+
+data Primitive = Primitive Name [Name] deriving (Eq, Show)
+
+data Primitives = Primitives [Primitive] deriving (Eq, Show)
+
+data CDDB = CDDB Name Version Date Primitives Rules deriving (Eq, Show)
 
 fieldNameParser :: Name -> IParser String
 fieldNameParser name = string name <* char ':' <* spaces <* optional commentParser
@@ -101,20 +132,58 @@ indentedFieldParser name parser = fmap snd $ indented *> fieldParser name parser
 indentedFieldOptionalParser:: Name -> IParser a -> IParser [a]
 indentedFieldOptionalParser name parser = option [] $ fmap snd $ indented *> fieldParser name parser
 
+ruleParser :: IParser Rule
+ruleParser = withPos $ do
+    _ <- fieldNameParser "rule"
+    comment <- indentedFieldOptionalDataParser "comment" noncommentStringParser ""
+    score <- indentedFieldOptionalDataParser "score" doubleParser 1.0
+    match <- indentedfieldDataParser "match" noncommentStringParser
+    primitives <- indentedFieldOptionalParser "primitives" pTaxonomy
+    actions <- indentedFieldOptionalDataParser "actions" noncommentStringParser ""
+    return $ Rule comment score match primitives actions
+
 rulesParser :: IParser Rules
 rulesParser = do
     (name, rules) <- fieldParser "rules" ruleParser
-    return $ Rules name rules
+    return $ Rules rules
 
-ruleParser :: IParser Rule
-ruleParser = withPos $ do
-    name <- fieldNameParser "rule"
-    comment <- indentedFieldOptionalDataParser "comment" noncommentString ""
-    score <- indentedFieldOptionalDataParser "score" doubleParser 1.0
-    match <- indentedfieldDataParser "match" noncommentString
-    primitives <- indentedFieldOptionalParser "primitives" pTaxonomy
-    actions <- indentedFieldOptionalDataParser "actions" noncommentString ""
-    return $ Rule name comment score match primitives actions
+primitiveParser :: IParser Primitive
+primitiveParser = withPos $ do
+    _ <- fieldNameParser "primitive"
+    name <- indentedfieldDataParser "name" noncommentStringParser
+    fields <- indentedfieldDataParser "fields" $ commaSeparatedparser noncommentStringParser
+    return $ Primitive name fields
+
+primitivesParsers :: IParser Primitives
+primitivesParsers = withPos $ do
+    (name, primitives) <- fieldParser "primitives" primitiveParser
+    return $ Primitives primitives
+
+aCDDBParsers :: IParser CDDB
+aCDDBParsers = do
+    many commentParser
+    name <- fmap snd $ fieldDataParser "name" noncommentStringParser
+    version <- fmap snd $ fieldDataParser "version" integerParser
+    date <- fmap snd $ fieldDataParser "date" dateParser
+    primitives <- primitivesParsers
+    rules <- rulesParser
+    return $ CDDB name version date primitives rules
+
+parseCDDBFile :: FilePath -> IO (Either ParseError CDDB)
+parseCDDBFile path = do
+    fileContent <- readFile path
+    let res = iParse aCDDBParsers path fileContent
+    return res
+
+
+
+
+
+
+
+
+
+
 
 -------------------
 pTerm :: IParser String
