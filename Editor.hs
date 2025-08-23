2d576e5c8d04a029887f384de5d66433eb5359f0
yantayga <>
2025-08-23 14:14:24 +0300

    Added more reading/writing editor setting

--- Editor.hs
+++ Editor.hs
@@ -10,13 +10,17 @@ import System.IO
 import Editor.Settings
 
 main :: IO ()
-main = runInputT defaultSettings loop
-   where
-       loop :: InputT IO ()
-       loop = do
-           minput <- getInputLine "CDDB> "
-           case minput of
-               Nothing -> return ()
-               Just "quit" -> return ()
-               Just input -> do outputStrLn $ "Input was: " ++ input
-                                loop
+main = do
+        settings <- readSettings
+        runInputTWithPrefs (haskelinePrefsFromSettings settings) (haskelineSettionsFromSettings settings) loop
+        writeSettings settings
+    where
+        loop :: InputT IO ()
+        loop = do
+            minput <- getInputLine "CDDB> "
+            case minput of
+                Nothing -> return ()
+                Just "quit" -> return ()
+                Just input -> do outputStrLn $ "Input was: " ++ input
+                                 loop
+                                
--- Editor/Settings.hs
+++ Editor/Settings.hs
@@ -6,18 +6,39 @@ module Editor.Settings where
 import GHC.Generics
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
-import System.IO
 
-data Settings = Settings deriving (Eq, Show, Generic, ToJSON, FromJSON)
+import System.IO
+import qualified System.Console.Haskeline as HL
+
+data Settings = Settings {
+        historyFile :: String,
+        autoAddHistory :: Bool
+    } deriving (Eq, Show, Generic, ToJSON, FromJSON)
+
+haskelinePrefsFromSettings :: Settings -> HL.Prefs
+haskelinePrefsFromSettings _ = HL.defaultPrefs
+
+haskelineSettionsFromSettings :: Settings ->  HL.Settings IO
+haskelineSettionsFromSettings settings = HL.defaultSettings {
+        HL.historyFile = Just $ historyFile settings,
+        HL.autoAddHistory = autoAddHistory settings
+    }
+
+defalultSettings :: Settings
+defalultSettings = Settings {
+        historyFile = ".cddb_history",
+        autoAddHistory = True
+    }
 
 decodeSettings :: B.ByteString -> Maybe Settings
 decodeSettings s = decode s
 
 readSettings :: IO (Settings)
 readSettings = do
-    fileContent <- B.readFile "editorSettings.json"
+    handle <- openFile "editorSettings.json" ReadWriteMode
+    fileContent <- B.hGetContents handle 
     case decodeSettings fileContent of
-        Nothing -> return Settings
+        Nothing -> return defalultSettings
         Just s -> return s
 
 writeSettings :: Settings -> IO ()
