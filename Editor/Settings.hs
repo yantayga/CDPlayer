46cbbddd7c24ff50c95e88613ad3c09352a923a7
yantayga <>
2025-08-23 13:12:11 +0300

    Implemented reading/writing editor setting

--- Editor/Settings.hs
+++ Editor/Settings.hs
@@ -5,5 +5,23 @@ module Editor.Settings where
 
 import GHC.Generics
 import Data.Aeson
+import qualified Data.ByteString.Lazy as B
+import System.IO
 
 data Settings = Settings deriving (Eq, Show, Generic, ToJSON, FromJSON)
+
+decodeSettings :: B.ByteString -> Maybe Settings
+decodeSettings s = decode s
+
+readSettings :: IO (Settings)
+readSettings = do
+    fileContent <- B.readFile "editorSettings.json"
+    case decodeSettings fileContent of
+        Nothing -> return Settings
+        Just s -> return s
+
+writeSettings :: Settings -> IO ()
+writeSettings settings = do
+    B.writeFile "editorSettings.json" $  encode (toJSON settings)
+
+
