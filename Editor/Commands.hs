51d7eface6aa8f08b958394ee1ec1d21fa6a7557
yantayga <>
2025-08-23 16:07:43 +0300

    Added some help for editor commands

--- Editor/Commands.hs
+++ Editor/Commands.hs
@@ -6,27 +6,37 @@ module Editor.Commands where
 import CDDB.Types
 import CDDB.Process
 
+import System.Console.Haskeline
+
 import qualified Data.Map as M
-import Data.List (intercalate)
+import Data.List (intercalate, isInfixOf)
 
 data ProgramState = ProgramState {
         cddb :: CDDB,
-        currentRule :: Maybe Rule,
-        isNotSaved :: Bool
+        cddbFileName :: String,
+        isNotSaved :: Bool,
+        currentRule :: Maybe Rule
     }
 
 type Arguments = [String]
 
 type Command = Arguments -> ProgramState -> Either String ProgramState
 
-commands :: M.Map String Command
+type HelpString = String
+
+data CommandDef = CommandDef Command HelpString
+
+commands :: M.Map String CommandDef
 commands = M.fromList [
-        ("test", cmdTestErMsg),
-        ("new", cmdCreateEmptyCDDB)
+        ("new",  CommandDef cmdCreateEmptyCDDB "Create new database."),
+        ("save", CommandDef cmdSaveCDDB "Save database with optional file name."),
+        ("load", CommandDef cmdLoadCDDB "Load database with optional file name."),
+        ("help", CommandDef cmdHelp "This help."),
+        ("test", CommandDef cmdTestErrMsg "Show test error message with arguments.")
     ]
 
 initialProgramState :: ProgramState
-initialProgramState = ProgramState emptyCDDB Nothing True
+initialProgramState = ProgramState {cddb = emptyCDDB, cddbFileName = "", isNotSaved = True, currentRule = Nothing}
 
 runCommand :: String -> ProgramState -> Either String ProgramState
 runCommand cmd state =
@@ -34,12 +44,26 @@ runCommand cmd state =
         [] -> Left "Empty command"
         (cmdName: args) ->
             case M.lookup cmdName commands of
-                Nothing -> Left $ "Command '" ++ cmdName ++ "' not found"
-                Just fn -> fn args state
+                Nothing -> Left $ "Command '" ++ cmdName ++ "' not found. Possible variants: " ++ intercalate " " (findMostSimilar cmdName)
+                Just (CommandDef fn _) -> fn args state
 
-cmdTestErMsg :: Command
-cmdTestErMsg args _= Left $ "TEST ERROR MESSAGE: " ++ intercalate " " args
+findMostSimilar :: String -> [String]
+findMostSimilar cmdname = (filter . isInfixOf) cmdname $ M.keys commands
+
+cmdTestErrMsg :: Command
+cmdTestErrMsg args _= Left $ "TEST ERROR MESSAGE: " ++ intercalate " " args
+
+cmdHelp :: Command
+cmdHelp args _ = Left $ M.foldrWithKey (addCommadHelp args) "Commands:\n" commands
+    where
+        addCommadHelp names key (CommandDef _ def) acc = acc ++ "\n" ++ key ++ "\n\t" ++ def
 
 cmdCreateEmptyCDDB :: Command
-cmdCreateEmptyCDDB args state = Right $ state { cddb = emptyCDDB, currentRule = Nothing, isNotSaved = True}
+cmdCreateEmptyCDDB args state = Right initialProgramState
+
+cmdSaveCDDB :: Command
+cmdSaveCDDB args state = undefined
+
+cmdLoadCDDB :: Command
+cmdLoadCDDB args state = undefined
 
