83c8ceb507cca08c618a7753be47553807e3e0dc
yantayga <227780093+yantayga@users.noreply.github.com>
2025-08-26 18:52:50 +0300

    Improved filter tree

--- CDDB/Tree/Filter.hs
+++ CDDB/Tree/Filter.hs
@@ -22,7 +22,8 @@ import CDDB.JSON
 data FilterExpressionT a = Asterisk a
     | FilterTag a TagId [FilterExpressionT a]
     | FilterWord a TagId String
-    | FilterNot (FilterExpressionT a )
+    | FilterNot (FilterExpressionT a) -- No variable binding for Not
+    | FilterOr a [FilterExpressionT a]
     deriving (Eq, Generic, Functor)
 
 type FilterExpression = FilterExpressionT (Maybe VariableName)
@@ -35,67 +36,92 @@ instance FromJSON FilterExpression where
 
 instance Show FilterExpression where
     show :: FilterExpression -> String
-    show (Asterisk Nothing) = "*"
-    show (Asterisk (Just vn)) = vn ++ ":*"
-    show (FilterTag Nothing id ts) = id ++ "[" ++ intercalate ", " (map show ts) ++ "]"
-    show (FilterTag (Just vn) id ts) = vn ++ ":" ++ id ++ "[" ++ intercalate ", " (map show ts) ++ "]"
-    show (FilterWord Nothing id w) = id ++ "(" ++ show w ++ ")"
-    show (FilterWord (Just vn) id w) = vn ++ ":" ++ id ++ "(" ++ show w ++ ")"
+    show (Asterisk v) = showVariable v ++ "*"
+    show (FilterTag v tagId ts) = showVariable v ++ tagId ++ "[" ++ intercalate ", " (map show ts) ++ "]"
+    show (FilterWord v tagId w) = showVariable v ++ tagId ++ "(" ++ show w ++ ")"
     show (FilterNot f) = "~" ++ show f
+    show (FilterOr v fs) = showVariable v ++ "{" ++ intercalate ", " (map show fs) ++ "}"
+
+showVariable :: Maybe String -> String
+showVariable Nothing = ""
+showVariable (Just vn) = vn ++ ":"
 
 instance Read FilterExpression where
-    readPrec = choice [readAsterisk, readAsteriskWithVariable, readTag, readTagWithVariable, readWord, readWordWithVariable]
+    readPrec = choice [readAsterisk, readAsteriskWithVariable, readTag, readWord, readNot, readOr]
         where
             readAsterisk = do
-                L.Symbol "*" <- lexP
+                expectP (L.Symbol "*")
                 return $ Asterisk Nothing
             readAsteriskWithVariable = do
                 L.Ident vn <- lexP
-                L.Symbol ":" <- lexP
-                L.Symbol "*" <- lexP
+                expectP (L.Symbol ":*")
                 return $ Asterisk $ Just vn
             readTag = do
+                v <- readVariable
                 L.Ident n <- lexP
-                l <- step readListPrec
-                return $ FilterTag Nothing n l
-            readTagWithVariable = do
-                L.Ident vn <- lexP
-                L.Symbol ":" <- lexP
-                L.Ident n <- lexP
-                l <- step readListPrec
-                return $ FilterTag (Just vn) n l
+                l <- readListPrec
+                return $ FilterTag v n l
             readWord = do
+                v <- readVariable
                 L.Ident n <- lexP
-                paren
-                    ( do
-                        L.String s <- lexP
-                        return $ FilterWord Nothing n s
-                    )
-            readWordWithVariable = do
-                L.Ident vn <- lexP
-                L.Symbol ":" <- lexP
-                L.Ident n <- lexP
-                paren
-                    ( do
-                        L.String s <- lexP
-                        return $ FilterWord (Just vn) n s
-                    )
+                expectP (L.Punc "(")
+                L.String s <- lexP
+                expectP (L.Punc ")")
+                return $ FilterWord v n s
+            readNot = do
+                expectP (L.Punc "~")
+                f <- readPrec :: ReadPrec FilterExpression
+                return $ FilterNot f
+            readOr = do
+                v <- readVariable
+                fs <- step $ cbList readPrec :: ReadPrec [FilterExpression]
+                return $ FilterOr v fs
+
     readListPrec = readListPrecDefault
+
+readVariable :: ReadPrec (Maybe String)
+readVariable = readJust +++ return Nothing
+    where
+        readJust = do
+            L.Ident vn <- lexP
+            expectP (L.Symbol ":")
+            return $ Just vn
+
+-- Taken form GHC.Read 'list'
+cbList :: ReadPrec a -> ReadPrec [a]
+cbList readx = do
+    expectP (L.Punc "{")
+    listRest False +++ listNext
+    where
+        listRest started = do
+            L.Punc c <- lexP
+            case c of
+                "}"           -> return []
+                "," | started -> listNext
+                _             -> pfail
+        listNext = do
+            x  <- reset readx
+            xs <- listRest True
+            return (x:xs)
 
 type VariablePaths = M.Map VariableName TreePath
 
 -- Very inefficirent
 -- TODO: look at Trie data type
--- TODO: add variable setting for *
+-- TODO: add variable setting for * and {}
+-- TODO: implement Not
+-- TODO: implement Or
 matchFilterExpr :: SyntacticTree -> FilterExpression -> Maybe VariablePaths
 matchFilterExpr t filterExpr = matchFilterExpr' t filterExpr 0 []
 
 matchFilterExpr' :: SyntacticTree -> FilterExpression -> TreePos -> TreePath -> Maybe VariablePaths
 matchFilterExpr' _ (Asterisk _) _ _ = Just M.empty
-matchFilterExpr' (Tag id ts) (FilterTag Nothing fid fs) pos path    = guard (id == fid) >> matchFilterExprs ts fs pos (pos:path)
-matchFilterExpr' (Tag id ts) (FilterTag (Just vn) fid fs) pos path  = guard (id == fid) >> matchFilterExprs ts fs pos (pos:path) >>= \vs -> Just (addPath vn pos path vs)
-matchFilterExpr' (Word id s) (FilterWord Nothing fid fs) pos _      = guard (id == fid && s == fs) >> Just M.empty
-matchFilterExpr' (Word id s) (FilterWord (Just vn) fid fs) pos path = guard (id == fid && s == fs) >> Just (addPath vn pos path M.empty)
+matchFilterExpr' (Tag tagId ts) (FilterTag Nothing fid fs) pos path    = guard (tagId == fid) >> matchFilterExprs ts fs pos (pos:path)
+matchFilterExpr' (Tag tagId ts) (FilterTag (Just vn) fid fs) pos path  = guard (tagId == fid) >> matchFilterExprs ts fs pos (pos:path) >>= \vs -> Just (addPath vn pos path vs)
+matchFilterExpr' (Word tagId s) (FilterWord Nothing fid fs) _ _        = guard (tagId == fid && s == fs) >> Just M.empty
+matchFilterExpr' (Word tagId s) (FilterWord (Just vn) fid fs) pos path = guard (tagId == fid && s == fs) >> Just (addPath vn pos path M.empty)
+matchFilterExpr' _ (FilterNot _) _ _  = error "Filter expression for 'Not' is not implemented yet..."
+matchFilterExpr' _ (FilterOr _ _) _ _ = error "Filter expression for 'Or' is not implemented yet..."
 matchFilterExpr' _ _ _ _ = Nothing
 
 addPath :: VariableName -> TreePos -> TreePath -> VariablePaths -> VariablePaths
@@ -106,7 +132,7 @@ matchFilterExprs [] [] _ _ = Just M.empty
 matchFilterExprs _ [] _ _ = Nothing
 matchFilterExprs [] ((Asterisk _): sfs) pos path = matchFilterExprs [] sfs pos path
 matchFilterExprs [] _ _ _ = Nothing
-matchFilterExprs ts@(t: sts) fs@((Asterisk _): sfs) pos path = matchFilterExprs ts sfs pos path <|> matchFilterExprs sts fs (pos + 1) path
+matchFilterExprs ts@(_: sts) fs@((Asterisk _): sfs) pos path = matchFilterExprs ts sfs pos path <|> matchFilterExprs sts fs (pos + 1) path
 matchFilterExprs (t: sts) (f: sfs) pos path = do
     vs1 <- matchFilterExpr' t f pos path
     vs2 <- matchFilterExprs sts sfs (pos + 1) path
